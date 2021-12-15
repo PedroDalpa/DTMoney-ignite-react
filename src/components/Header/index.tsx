1f80c793337564dcbbeb4a73104548d89b9e9009
Pedro Dalpa <pedro380dalpa@gmail.com>
2021-12-15 16:37:40 -0300

    feat: create modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeadersProps {
+  handleOpenNewTransactionModal: () => void
+}
+
+export function Header({ handleOpenNewTransactionModal }: HeadersProps) {
+
   return (
     <Container>
       <Content>
 
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>
           Nova transação
         </button>
+
       </Content>
     </Container>
   )
